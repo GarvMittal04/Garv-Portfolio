92761a02af08ae567245d7e32fc1cc7ff38e9bfb
Garv Mittal <121634148+GarvMittal04@users.noreply.github.com>
2025-04-21 20:06:41 +0530

    Add files via upload

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import pdf from '..pdf/resume.pdf'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <div className="container home">
+
+      <div className="left">
+        <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit cumque neque expedita ab? Est minus autem, nisi cupiditate sapiente ipsa velit soluta. Fuga animi architecto rem commodi a aut deserunt.</h1>
+      
+      <a href="" className="btn btn-outline-warning">Download Resume</a>
+      
+      </div>
+      <div className="right">
+        <div className="img">
+        <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
