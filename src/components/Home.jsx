59bf3504b08d49096ee8ef15ecf5460cf760a6de
Garv Mittal <121634148+GarvMittal04@users.noreply.github.com>
2025-04-24 02:01:29 +0530

    Add files via upload

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import pdf from "../pdf/resume.pdf";
+import hero from './data/hero.json';
+
 const Home = () => {
   return (
     <>
@@ -8,12 +10,12 @@ const Home = () => {
       <div className="left">
         <h1>This is a Garv Mittal's Portfolio which is under contruction .</h1>
       
-      <a href={pdf} download="resume..pdf" className="btn btn-outline-warning">Download Resume</a>
+      <a href={pdf} download="Resume.pdf" className="btn btn-outline-warning">Download Resume</a>
       
       </div>
       <div className="right">
         <div className="img">
-        <img src="" alt="" />
+        <img src={`/assets/${hero.imgSrc}`} alt="hero" />
         </div>
       </div>
     </div>
