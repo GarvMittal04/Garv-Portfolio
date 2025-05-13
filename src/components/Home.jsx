6df421dec8aaab1ae6a2a5b3e34b27d146217976
Garv Mittal <121634148+GarvMittal04@users.noreply.github.com>
2025-05-13 15:33:40 +0530

    Add files via upload

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,41 @@
 import React, { useEffect, useRef } from "react";
 import pdf from "../pdf/resume.pdf";
 import hero from './data/hero.json';
+import Typed from "typed.js";
 
 const Home = () => {
+  const typedRef = useRef(null)
+  useEffect(() => {
+  
+    const options = {
+      strings:["Welcome to My Protfolio","My Name is Garv Mittal","I'm a Frontend Developer","Web Developer(React)"],
+    typeSpeed:50,
+    backSpeed:50,
+    loop:true
+    }
+
+    const typed = new Typed(typedRef.current, options)
+    return () => {
+      typed.destroy()
+    }
+  }, [])
+  
   return (
     <>
-    <div className="container home">
+    <div className="container home" id="home">
 
-      <div className="left">
-        <h1>This is a Garv Mittal's Portfolio website which is under construction .</h1>
+      <div className="left"
+      data-aos="fade-up-right"
+      data-aos-duration="1000" >
+        <h1 ref={typedRef}></h1>
       
-      <a href={pdf} download="Resume.pdf" className="btn btn-outline-warning">Download Resume</a>
+      <a href={pdf} download="Resume.pdf" className="btn btn-outline-warning my-3">Download Resume</a>
       
       </div>
-      <div className="right">
+      <div className="right"
+      
+      data-aos="fade-up-left"
+      data-aos-duration="1000" >
         <div className="img">
         <img src={`/assets/${hero.imgSrc}`} alt="hero" />
         </div>
